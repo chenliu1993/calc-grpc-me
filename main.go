2028d2af7d2deef56ad1cb0f2817e3e19edba0f3
chenliu1993 <13630583107@163.com>
2022-04-25 11:11:50 +0800

    register more than one informer through informer factory

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/runtime"
@@ -34,18 +35,29 @@ func main() {
 		log.Panic(err.Error())
 	}
 
-	factory := informers.NewSharedInformerFactory(clientset, 0)
-	informer := factory.Core().V1().PersistentVolumes().Informer()
+	informersSyncd := make([]cache.InformerSynced, 0)
+
+	factory := informers.NewSharedInformerFactory(clientset, time.Hour*24)
+	pvinformer := factory.Core().V1().PersistentVolumes().Informer()
+	informersSyncd = append(informersSyncd, pvinformer.HasSynced)
+	pvcinformer := factory.Core().V1().PersistentVolumeClaims().Informer()
+	informersSyncd = append(informersSyncd, pvcinformer.HasSynced)
+
 	stopper := make(chan struct{})
 	defer close(stopper)
 	defer runtime.HandleCrash()
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    onAdd,
-		UpdateFunc: onUpdate,
-		DeleteFunc: nil,
+	pvinformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    onPVAdd,
+		UpdateFunc: onPVUpdate,
+		DeleteFunc: onPVDelete,
 	})
-	go informer.Run(stopper)
-	if !cache.WaitForCacheSync(stopper, informer.HasSynced) {
+	pvcinformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    onPVCAdd,
+		UpdateFunc: onPVCUpdate,
+		DeleteFunc: onPVCDelete,
+	})
+	go factory.Start(stopper)
+	if !cache.WaitForCacheSync(stopper, informersSyncd...) {
 		runtime.HandleError(errors.New("timed out waiting for caches to sync"))
 		return
 	}
@@ -54,14 +66,38 @@ func main() {
 
 // onAdd is the function executed when the kubernetes informer notified the
 // presence of a new kubernetes node in the cluster
-func onAdd(obj interface{}) {
+func onPVAdd(obj interface{}) {
 	// Cast the obj as pv
 	pv := obj.(*corev1.PersistentVolume)
-	fmt.Printf("[onAdd] pv %s with claim %s in %s\n", pv.Name, fmt.Sprintf("%s/%s", pv.Spec.ClaimRef.Namespace, pv.Spec.ClaimRef.Name), pv.Status.Phase)
+	fmt.Printf("[onPVAdd] pv %s with claim %s in %s\n", pv.Name, fmt.Sprintf("%s/%s", pv.Spec.ClaimRef.Namespace, pv.Spec.ClaimRef.Name), pv.Status.Phase)
 }
 
-func onUpdate(_, obj interface{}) {
+func onPVUpdate(_, obj interface{}) {
 	// Cast the obj as pv
 	pv := obj.(*corev1.PersistentVolume)
-	fmt.Printf("[onUpdate] pv %s with claim %s in %s\n", pv.Name, fmt.Sprintf("%s/%s", pv.Spec.ClaimRef.Namespace, pv.Spec.ClaimRef.Name), pv.Status.Phase)
+	fmt.Printf("[onPVUpdate] pv %s with claim %s in %s\n", pv.Name, fmt.Sprintf("%s/%s", pv.Spec.ClaimRef.Namespace, pv.Spec.ClaimRef.Name), pv.Status.Phase)
+}
+
+func onPVDelete(obj interface{}) {
+	pv := obj.(*corev1.PersistentVolume)
+	fmt.Printf("[onPVDelete] pv %s with claim %s in %s deleted\n", pv.Name, fmt.Sprintf("%s/%s", pv.Spec.ClaimRef.Namespace, pv.Spec.ClaimRef.Name), pv.Status.Phase)
+}
+
+func onPVCAdd(obj interface{}) {
+	// Cast the obj as pv
+	pvc := obj.(*corev1.PersistentVolumeClaim)
+	fmt.Printf("[onPVCAdd] pvc %s in %s\n", pvc.Name, pvc.Status.Phase)
+}
+
+func onPVCUpdate(old, obj interface{}) {
+	// Cast the obj as pv
+	pvc := obj.(*corev1.PersistentVolumeClaim)
+	oldobj := old.(*corev1.PersistentVolumeClaim)
+	fmt.Printf("[onPVCUpdate] old pvc %s in %s while new pvc %s in %s\n", oldobj.Name, oldobj.Status.Phase, pvc.Name, pvc.Status.Phase)
+}
+
+func onPVCDelete(obj interface{}) {
+	// Cast the obj as pv
+	pvc := obj.(*corev1.PersistentVolumeClaim)
+	fmt.Printf("[onPVCDelete] pvc %s in %s deleted\n", pvc.Name, pvc.Status.Phase)
 }
